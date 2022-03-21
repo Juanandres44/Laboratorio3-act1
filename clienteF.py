eadfbe8ef23b1f568a53534750aadd9bb2cc2a46
Juanandres44 <ja.santiago>
2022-03-20 19:52:52 -0500

    cambios

--- clienteF.py
+++ clienteF.py
@@ -7,60 +7,50 @@ from datetime import datetime
 import time
 
 num_client = int(input('Cuantos clientes desea crear? '))
-while (num_client>25 and num_client <= 0):
+while (num_client <= 0 and num_client>25 ):
     num_client = int(input('Por favor ingresar un número válido: '))
-
 
 class Main:    
     def __init__(self):
         self.lock = threading.Lock()
-    def cliente_funct(self, nombre):
+    def funct(self, nombre):
         self.lock.acquire()
-        
         log = open("./logs/"+nombre+' '+datetime.today().strftime('%Y-%m-%d-%H-%M-%S')+"log.txt", "w")
         self.lock.release()
+
         # Crear a TCP
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
-        # Conectar socket al puerto donde se esta escuchando
-        server_address = ('192.168.37.133', 8888)
-        print( 'connecting to %s port %s' % server_address)
-        sock.connect(server_address)
+        # Conectar socket al puerto donde se esta escuchando. IP de maquina virtual Ubuntu.
+        server_addr = ('192.168.37.133', 8888)
+        print( 'connecting to %s port %s' % server_addr)
+        sock.connect(server_addr)
         file = open("./ArchivosRecibidos/"+nombre+"-prueba-"+str(num_client)+".txt", "w")
-       
         
         try:
-            
-            # Enviar datos
+
             mensaje = b'Iniciar conexion...'
             print ( 'enviando "%s"' % mensaje)
             sock.sendall(mensaje)
         
-            # buscar la respuesta
-
             confirmacion = sock.recv(32)
             print(confirmacion.decode('utf-8'))
-            #hash a comparar
             md5 = hashlib.md5()
             if(confirmacion.decode('utf-8') == "ok"):
                 sock.sendall(b'Cual es el nombre del archivo?')
                 nA = sock.recv(32)
                 nombreArchivo = nA.decode('utf-8') 
                 log.write('El nombre es: '+nombreArchivo+'\n')
-                
                 sock.sendall(b'listo')
                 self.lock.acquire()
                 hash = sock.recv(32)
                 print('Hash enviado por el servidor TCP:',hash.decode('utf-8'))
                 sock.sendall(b'Hash recibido')
                 self.lock.release()
-                
-                
                 num_paq=0
                 start = time.time()
 
-                while (True):
-                       
+                while (True):      
                     data = sock.recv(1024)
                     
                     if data:
@@ -70,7 +60,7 @@ class Main:
                             num_paq+=1
                             
                         except:
-                            print('Hubo Error') 
+                            print('Ocurrio un error') 
                             sock.sendall(b'Hubo un error al recibir el archivo')
                             break
                         
@@ -79,18 +69,15 @@ class Main:
                         sock.sendall(b'Archivo recibido')
                         break
                 end = time.time()
-                
                 tam = os.path.getsize("./archivosRecibidos/"+nombre+"-prueba-"+str(num_client)+".txt")
                 
+
                 file.close()
                 log.write('El nombre del cliente es: '+nombre+'\n')
                 log.write('El tamaño del archivo es: '+str(tam/1000000)+' MB'+'\n')
                 
-               
-               
                 print("Hash del archivo leido: {0}".format(md5.hexdigest()))
                   
-                
                 if(hash.decode('utf-8') == md5.hexdigest()):
                     print("Archivo leido")
                     log.write('Entrega del archivo'+'\n')
@@ -100,23 +87,20 @@ class Main:
                 log.write('Tiempo de transferencia: '+str(end-start)+ ' segs'+'\n') 
                 log.write('Valor total en bytes recibidos: '+str(tam)+'\n')     
                 log.write('Cantidad de paquetes recibidos: '+str(num_paq)+'\n') 
-                
-                
+                       
         finally:
             print ('Cerrar socket')
             sock.close()
             print ('Fin del programa')
             log.close()
             
-            
-
-def worker(c, nombre):
-        c.cliente_funct(nombre)
+def targ(c, nombre):
+        c.funct(nombre)
         
 hilo=Main()
 for num_cliente in range(num_client):
     cliente = threading.Thread(name="Cliente%s" %(num_cliente+1),
-                               target=worker,
+                               target=targ,
                                args=(hilo,"Cliente%s" %(num_cliente+1))
                               )
     cliente.start()
--- serverF.py
+++ serverF.py
@@ -5,21 +5,21 @@ import hashlib
 import time
 from datetime import datetime
 
-# Crear socket tcp/ip
+# Crear socket TCP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 num_conn = int(input('Bienvenido al servidor TCP. Por favor ingresar la cantidad de conexiones: '))
 
-while (num_conn>25 and num_conn <= 0):
+while (num_conn <= 0 and num_conn>25):
     num_conn = int(input('Por favor ingresar un número válido: '))
 
 
-# conectar socket al puerto
+# Conectar socket al puerto con su IP respectiva.
 server_address = ('192.168.37.133', 8888)
 print('El %s esta esparando en el puerto %s' % server_address)
 sock.bind(server_address)
 
-#archivo a transmitir
+#Archivo a enviar
 file = input('Por favor ingrese el nombre del archivo a enviar. ej: Prueba_100MB.txt o Prueba_250MB.txt ')
 while file not in ['Prueba_100MB.txt','Prueba_250MB.txt']:
     file = input('Por favor ingresar un nombre de archivo correcto: ')
@@ -37,8 +37,7 @@ md5 = hashlib.md5()
 while(buf):
     md5.update(buf)
     buf = archivo.read(1024)
-
-  
+ 
 sock.listen(25)
 
 for i in range(num_conn):
@@ -53,22 +52,16 @@ for i in range(num_conn):
     try:
         print ('Conectado de', client_addr)
         
-        data = connection.recv(32) 
-            
+        data = connection.recv(32)    
         connection.sendall(b'ok')
-        
         connection.recv(32)
         connection.sendall(bytes(file, 'utf-8'))
-        
         data = connection.recv(32)
-        
         print(data.decode('utf-8'))
-        # Recibir datos y retransmitirlos
-       
+
         if(data.decode('utf-8')== "listo"):
             
             connection.sendall(bytes(md5.hexdigest(), 'utf-8'))
-            #Enviamos linea por linea el archivo
             recibido = connection.recv(32)
             if(recibido.decode('utf-8') == 'Hash recibido'):
                 while (l):
@@ -76,7 +69,6 @@ for i in range(num_conn):
                     l= f.read(1024)
             
                 connection.send(l)
-                #Enviamos el hash
                 print('Conexión terminada exitosamente')
                 log.write('Entrega del archivo: se envio el archivo '+'\n')  
                 end = time.time()
@@ -84,5 +76,4 @@ for i in range(num_conn):
         log.write('Tiempo transferencia con cliente: '+str(end-start)+'\n')  
             
     finally:
-        # cerrar coneccion
         connection.close()
